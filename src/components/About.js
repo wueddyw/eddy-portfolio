6b7ce8c36abc097b16eeaa12adccff2c7b201a1a
Eddy Wu <56403971+wueddyw@users.noreply.github.com>
2024-09-07 14:18:22 -0700

    changed image position in about me section

--- src/components/About.js
+++ src/components/About.js
@@ -4,23 +4,27 @@ import me from '../assets/me1.jpg'
 
 const About = () => {
   return (
+    
     <div className="about-section">
-
+      <h2 className="about-header">About Me</h2>
       
 
-      <h2 className="about-container">About Me</h2>
-      <div className="about-image-container">
-        <img src={me} alt="Eddy Wu" className="about-image" />
+      <div className="about-container">
+        <div className="about-image-container">
+          <img src={me} alt="Eddy Wu" className="about-image" />
+        </div>
+        <div className="about-content-container">
+        <p>
+          Hi, I'm Eddy. I'm graduating from the Bachelor of Science in Applied Computer Science (Network Security Option) 
+          program at BCIT in December 2026. I am taking this program as a continuation of my diploma in Computer Systems Technology (Client/Server Option).
+          I'm currently going into my second term.
+        </p>
+        <p>
+          I've worked on several projects including web applications and APIs, and I'm confident with Python, C, SQL, Django, and React.js. 
+          I'm looking for a full-time co-op placement for January 2025 so if you'd like to connect, send me an email.
+        </p>
+        </div>
       </div>
-      <p className="about-content-container">
-        Hi, I'm Eddy. I'm graduating from the Bachelor of Science in Applied Computer Science (Network Security Option) 
-        program at BCIT in December 2026. I am taking this program as a continuation of my diploma in Computer Systems Technology (Client/Server Option).
-        I'm currently going into my second term.
-      </p>
-      <p className="about-content-container">
-        I've worked on several projects including web applications and APIs, and I'm confident with Python, C, SQL, Django, and React.js. 
-        I'm looking for a full-time co-op placement for January 2025 so if you'd like to connect, send me an email.
-      </p>
     </div>
   );
 };
